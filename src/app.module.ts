ec3eaaa5290b869ca2857b59f948a8b3e9d0d2cc
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-03-18 02:40:31 -0400

    test env file and new entity import

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,23 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
-import { UserEntity } from './user/interface/user.entity';
+import { UserEntity } from './user/class/user.entity';
 import { createTableUser1678941849551 as migrateUser } from './migration/1678941849551-create-table-user';
 import { createTableState1678944104732 as migrateState } from './migration/1678944104732-create-table-state';
 import { createTableCity1678944183972 as migrateCity } from './migration/1678944183972-create-table-city';
 import { createTableAdress1678944201592 as migrateAddress } from './migration/1678944201592-create-table-adress';
+import { insertIntoState1679115176970 as migrateInsertState } from './migration/1679115176970-insert-into-state';
+import { insertIntoCity1679115185576 as migrateInsertCity } from './migration/1679115185576-insert-into-city';
+import { StateModule } from './state/state.module';
+import { StateEntity } from './state/class/state.entity';
+import { CityModule } from './city/city.module';
+import { cityEntity } from './city/class/city.entity';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: ['.development.env.local'],
+      isGlobal: true,
+      envFilePath: '.development.env.local',
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
@@ -19,11 +27,20 @@ import { createTableAdress1678944201592 as migrateAddress } from './migration/16
       password: 'root',
       port: 5432,
       username: 'postgres',
-      entities: [UserEntity],
-      migrations: [migrateUser],
+      entities: [UserEntity, StateEntity, cityEntity],
+      migrations: [
+        migrateUser,
+        migrateState,
+        migrateCity,
+        migrateAddress,
+        migrateInsertState,
+        migrateInsertCity,
+      ],
       migrationsRun: true,
     }),
     UserModule,
+    StateModule,
+    CityModule,
   ],
   controllers: [],
   providers: [],
